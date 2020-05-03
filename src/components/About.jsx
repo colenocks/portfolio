5a207763b7d5fc93c0b4b478c889f9d0b078a558
colenocks <enockscoleman@gmail.com>
2020-05-03 13:24:50 +0100

    feat: add "About" component contents and styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,30 @@ class About extends Component {
     return (
       <div>
         <Navbar />
-        <div>This is About</div>
+        <main className="about">
+          <div className="title">
+            <h2 className="lg-heading">
+              About <span className="text-secondary">Me</span>
+            </h2>
+          </div>
+          <h3 className="sm-heading">Here are a few information about me...</h3>
+          <div className="about-info">
+            <img
+              className="bioimage"
+              src={require("../assets/background-note.jpg")}
+              alt="Coleman Enocks"
+            />
+            <div className="bio">
+              <h3 className="text-secondary">BIO</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Incidunt architecto earum itaque tempora eaque atque ab ex.
+                Voluptate tenetur odit, itaque ab quisquam ex incidunt dolorem,
+                deleniti suscipit, ipsa voluptatum!
+              </p>
+            </div>
+          </div>
+        </main>
         <Footer />
       </div>
     );
