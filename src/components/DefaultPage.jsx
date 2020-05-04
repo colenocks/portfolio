256820835b2c9c10bafd6deb5a2f0d70df31b399
colenocks <enockscoleman@gmail.com>
2020-05-04 12:13:51 +0100

    style: center contents

--- src/components/DefaultPage.jsx
+++ src/components/DefaultPage.jsx
@@ -3,16 +3,20 @@ import React, { Component } from "react";
 class DefaultPage extends Component {
   render() {
     return (
-      <div className="container text-title">
-        <h1 className="my-4">404 error: page not found</h1>
-        <h5>
-          the requested url:{" "}
-          <span className="text-danger">
-            "{this.props.location.pathname}" was not found
-          </span>
-          {}
-        </h5>
-      </div>
+      <main>
+        <div className="flex-center title">
+          <h1 className="lg-heading">404 error: page not found</h1>
+        </div>
+        <div className="flex-center">
+          <h3>
+            the requested url:{" "}
+            <span className="text-secondary">
+              "{this.props.location.pathname}"
+            </span>{" "}
+            was not found
+          </h3>
+        </div>
+      </main>
     );
   }
 }
