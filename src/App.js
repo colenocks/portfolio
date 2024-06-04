3256a93cfc11509ee2467ab8bfad875e3064e726
colenocks <enockscoleman@gmail.com>
2024-06-04 13:21:44 +0100

    fix deprecations and warnings

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Techstack from "./components/Techstack";
-import Projects from "./components/Projects";
-import Blog from "./components/Blog";
-import DefaultPage from "./components/DefaultPage";
-import "./App.scss";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Techstack from './components/Techstack';
+import Projects from './components/Projects';
+import Blog from './components/Blog';
+import DefaultPage from './components/DefaultPage';
+import './App.scss';
 
 function App() {
   return (
     <div className="wrapper">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/techstack" component={Techstack} />
-        <Route exact path="/projects" component={Projects} />
-        <Route exact path="/blog" component={Blog} />
-        <Route exact path="/contact" component={Contact} />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/techstack" element={<Techstack />} />
+        <Route exact path="/projects" element={<Projects />} />
+        <Route exact path="/blog" element={<Blog />} />
+        <Route exact path="/contact" element={<Contact />} />
         <Route component={DefaultPage} />
-      </Switch>
+      </Routes>
     </div>
   );
 }
