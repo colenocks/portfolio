0831e81549fdc31617932b65697db129559f81d0
colenocks <enockscoleman@gmail.com>
2024-06-04 16:17:06 +0100

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react';
+import Home from './components/Home';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders home page', () => {
+  render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
+  const text = screen.getByText(/home/i);
+  expect(text).toBeInTheDocument();
 });
